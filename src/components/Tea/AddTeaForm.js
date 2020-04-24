d87e4cf884930170d75ec9208084bb8119162319
Tiffany Siu <tsiu88@gmail.com>
2020-04-24 15:02:07 -0700

    Add to control file

--- src/components/Tea/AddTeaForm.js
+++ src/components/Tea/AddTeaForm.js
@@ -1,11 +1,105 @@
 import React from "react";
+import propTypes from "prop-types";
 
 function AddTeaForm(props){
+
+  function handleFormSubmission(event){
+    event.preventDefault();
+    props.onAddTea({
+      name: event.target.name.value,
+      type: event.target.type.value,
+      origin: event.target.origin.value,
+      flavor: event.target.flavor.value,
+      price: event.target.price.value,
+      amount: event.target.amount.value,
+      image: event.target.image.value,
+    })
+  }
+
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+      <form onSubmit= {handleFormSubmission}>
+        <div className="form-group">
+          <label htmlFor="name">
+            Name:
+            <input
+              className="form-control"
+              name="text"
+              name="name"
+              placeholder="Jasmine, Matcha..." 
+              required/>
+          </label>
+        </div>
+        <div className="form-group">
+          <label htmlFor="type">
+            Type:
+            <input
+              className="form-control"
+              type="text"
+              name="type"
+              placeholder="Black, Green, White..." />
+          </label>
+        </div>
+        <div className="form-group">
+          <label htmlFor="origin">
+            Origin:
+            <input
+              className="form-control"
+              type="text"
+              name="origin"
+              placeholder="China, India, Japan..." />
+          </label>
+        </div>
+        <div className="form-group">
+          <label htmlFor="flavor">
+            Flavor:
+            <input
+              className="form-control"
+              type="text"
+              name="flavor"
+              placeholder="Smooth tea with sweet undertones..." />
+          </label>
+        </div>
+        <div className="form-group">
+          <label htmlFor="price">
+            Price:
+            <input
+              className="form-control"
+              type="number"
+              name="price"
+              placeholder="Price for a small tin of tea in dollars..."
+              required />
+          </label>
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">
+            Stock:
+            <input
+              className="form-control"
+              type="number"
+              name="amount"
+              placeholder="Number of tins of tea in stock..." 
+              required />
+          </label>
+        </div>
+        <div className="form-group">
+          <label htmlFor="image">
+            Image URL:
+            <input
+              className="form-control"
+              type="text"
+              name="image"
+              placeholder="Smooth tea with sweet undertones..." />
+          </label>
+        </div>
+        <button className="btn btn-light" type="submit">Add </button>
+      </form>
     </React.Fragment>
   );
 }
+
+AddTeaForm.propTypes = {
+  onAddTea: propTypes.func,
+};
 
 export default AddTeaForm;
--- src/components/Tea/TeaControl.js
+++ src/components/Tea/TeaControl.js
@@ -2,26 +2,119 @@ import React from 'react';
 import AddTeaForm from './AddTeaForm';
 import TeaList from './TeaList';
 
+const masterTeaList = [
+  {
+    name: "Dragonwell",
+    type: "Green Tea",
+    origin: "China",
+    flavor: "Mellow, floral flavor with slightly sweet aftertaste",
+    price: 10,
+    amount: 42,
+    image: "https://cdn.shopify.com/s/files/1/0888/8900/products/7004-Dragonwell_House-1.jpg?v=1445269617",
+  },
+  {
+    name: "Ti Kwan Yin",
+    type: "Oolong Tea",
+    origin: "China",
+    flavor: "Delicate, green, floral, sweet with mineral note",
+    price: 8,
+    amount: 54,
+    image: "https://cdn.shopify.com/s/files/1/0888/8900/products/Standard_Shot_Ti_Kwan_Yin.jpg?v=1548603635",
+  },
+  {
+    name: "Sheng Puer Pearls 2014",
+    type: "Puer Tea",
+    origin: "China",
+    flavor: "Sweet peach and evergreen notes, smooth with decent body that gets better with age",
+    price: 40,
+    amount: 7,
+    image: "https://cdn.shopify.com/s/files/1/0888/8900/products/Standard_Shot_Ms._Zhao_s_Sheng_Puer_Pearl.jpg?v=1554923461",
+  },
+  {
+    name: "English Breakfast",
+    type: "Black Tea Blend",
+    origin: "Assam, Ceylon, Kenya",
+    flavor: "Full bodied brew, strong and moderately caffeinated.",
+    price: 12,
+    amount: 35,
+    image: "https://cdn.shopify.com/s/files/1/0888/8900/products/9000-Decaf_English_Breakfast.jpg?v=1445269330",
+  },
+  {
+    name: "Yin Zhen",
+    type: "White Tea",
+    origin: "China",
+    flavor: "Sweet, creamy, silky tea",
+    price: 41,
+    amount: 7,
+    image: "https://cdn.shopify.com/s/files/1/0888/8900/products/Standard_Shot_8501_2019_Silver_Needles-1.jpg?v=1571847096",
+  },
+  {
+    name: "Fu Cha",
+    type: "Dark Tea Brick",
+    origin: "China",
+    flavor: "Sweet, mild, smooth, slight hint of fresh grass",
+    price: 17,
+    amount: 36,
+    image: "https://cdn.shopify.com/s/files/1/0888/8900/products/IG_posts_-_2020-01-10T105334.704.png?v=1578675295",
+  },
+  {
+    name: "Gyokuro",
+    type: "Green Tea",
+    origin: "Japan",
+    flavor: "Sweet, seaweed-y, with brothy body",
+    price: 28,
+    amount: 18,
+    image: "https://cdn.shopify.com/s/files/1/0888/8900/products/7701-Gyokuro.jpg?v=1445451386",
+  },
+  {
+    name: "Darjeeling",
+    type: "Black Tea",
+    origin: "India",
+    flavor: "Medium bodied, smooth, hints of muscatel, fresh taste with delicate nuances in flavor",
+    price: 6,
+    amount: 85,
+    image: "https://cdn.shopify.com/s/files/1/0888/8900/products/1000-TeaSource_Darjeeling-1.jpg?v=1446672895",
+  },
+  {
+    name: "Green Dragon",
+    type: "Oolong Tea",
+    origin: "Taiwan",
+    flavor: "Silky, medium bodied, sweet and fruity notes, light lingering aftertaste",
+    price: 11,
+    amount: 56,
+    image: "https://cdn.shopify.com/s/files/1/0888/8900/products/6405-Green_Dragon.jpg?v=1445450870",
+  },
+];
+
 class TeaControl extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
       formVisibleOnPage: false,
+      masterTeaList: masterTeaList,
     };
   }
 
+  handleToggleForms = () => {
+    this.setState(prevState => ({formVisibleOnPage: !prevState.formVisibleOnPage}));
+  }
+
   render(){
-    
     let currentlyVisibleState = null;
+    let buttonText = null;
     if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <AddTeaForm />
+      buttonText = "Return to Tea List"
     } else {
       currentlyVisibleState = <TeaList />
+      buttonText = "Add tea";
     }
     return (
       <React.Fragment>
+        <button onClick={this.handleToggleForms}>{buttonText}</button>
         {currentlyVisibleState}
+        
       </React.Fragment>
       
     )
